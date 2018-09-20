4af929cf3e62b4bcc9173885af110664160679a2
MSI\steviegiovanni <steviegiovanni@gmail.com>
2018-09-20 16:07:29 -0700

    initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+namespace Com.MyCompany.MyGame
+{
+    public class GameManager : Photon.PunBehaviour
+    {
+        #region Photon Messages
+        public override void OnPhotonPlayerConnected(PhotonPlayer newPlayer)
+        {
+            Debug.Log("OnPhotonPlayerConnected() " + newPlayer.NickName);
+            if (PhotonNetwork.isMasterClient)
+            {
+                Debug.Log("OnPhotonPlayerConnected isMasterClient " + PhotonNetwork.isMasterClient);
+                LoadArena();
+            }
+        }
+
+        public override void OnPhotonPlayerDisconnected(PhotonPlayer otherPlayer)
+        {
+            Debug.Log("OnPhotonPlayerDisconnected() " + otherPlayer.NickName);
+            if (PhotonNetwork.isMasterClient)
+                Debug.Log("OnPhotonPlayerDisconnected isMasterClient " + PhotonNetwork.isMasterClient);
+            LoadArena();
+        }
+
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+        #endregion
+
+        #region Public Methods
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+        #endregion
+
+        #region Private Methods
+        void LoadArena()
+        {
+            if (!PhotonNetwork.isMasterClient)
+            {
+                Debug.LogError("PhotonNetwork: Trying to load a level but we are not the master Client");
+            }
+            Debug.Log("PhotonNetwork: Loading Level: " + PhotonNetwork.room.PlayerCount);
+            PhotonNetwork.LoadLevel("Room for " + PhotonNetwork.room.PlayerCount);
+        }
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.MyCompany.MyGame
+{
+    public class Launcher : Photon.PunBehaviour
+    {
+        #region Public Variables
+        public PhotonLogLevel LogLevel = PhotonLogLevel.Informational;
+        public byte MaxPlayersPerRoom = 4;
+        public GameObject controlPanel;
+        public GameObject progressLabel;
+
+        #endregion
+
+        #region Private Variables
+        string _gameVersion = "1";
+        bool isConnecting;
+
+        #endregion
+
+        #region MonoBehaviour Callbacks
+
+        private void Awake()
+        {
+            PhotonNetwork.autoJoinLobby = false;
+            PhotonNetwork.automaticallySyncScene = true;
+            PhotonNetwork.logLevel = LogLevel;
+        }
+
+        private void Start()
+        {
+            progressLabel.SetActive(false);
+            controlPanel.SetActive(true);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Connect()
+        {
+            isConnecting = true;
+            progressLabel.SetActive(true);
+            controlPanel.SetActive(false);
+
+            if (PhotonNetwork.connected)
+                PhotonNetwork.JoinRandomRoom();
+            else
+                PhotonNetwork.ConnectUsingSettings(_gameVersion);
+        }
+
+        #endregion
+
+        #region Photon.PunBehaviour Callbacks
+
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("Launcher: OnConnectedToMaster() was called by PUN");
+            if(isConnecting)
+                PhotonNetwork.JoinRandomRoom();
+        }
+
+        public override void OnDisconnectedFromPhoton()
+        {
+            progressLabel.SetActive(true);
+            controlPanel.SetActive(false);
+            Debug.LogWarning("Launcher: OnDisconnectedFromPhoton() was called by PUN");
+        }
+
+        public override void OnPhotonRandomJoinFailed(object[] codeAndMsg)
+        {
+            Debug.Log("Launcher: OnPhotonRandomJoinFailed() was called by PUN. No random room available, so we create one.\nCaling: PhotonNetwork.CreateRoom(nukk, new RoomOptions(){maxPlayers = 4}, null);");
+            PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = MaxPlayersPerRoom }, null);
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("We load the 'Room for 1'");
+            PhotonNetwork.LoadLevel("Room for 1");
+        }
+
+        #endregion
+    }
+}
